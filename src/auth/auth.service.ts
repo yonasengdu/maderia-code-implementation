7a9da21256674463bfe861e311565be87eb889ad
yonasengdu <yonasengdu1994.com>
2022-12-04 21:57:59 +0300

    I instantiated the prismaService by creating new constructor in the auth.service class to start working on the validation logics

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from "@nestjs/common";
-
-@Injectable({})
+import { PrismaService } from "src/prisma/prisma.service";
+@Injectable()
 export class AuthService{
+    constructor(private prisma: PrismaService){}
 
     signup(){
         return  {'msg': 'i am signed up'}

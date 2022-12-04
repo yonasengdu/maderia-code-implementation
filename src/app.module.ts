bc42d9401947db349b6c5b5a138505e857907f23
yonasengdu <yonasengdu1994.com>
2022-12-04 21:54:00 +0300

    the 'app.module' class imports the 'PrismaModule' which is now available to all modules in the app. so I imported the PrismaModule in the app.module to made it available to all modules.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { ClientModule } from './client/client.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 
 
 @Module({
-  imports: [AuthModule, ClientModule],
+  imports: [AuthModule, ClientModule, PrismaModule],
 
 })
 export class AppModule {}

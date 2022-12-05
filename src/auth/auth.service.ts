3214c7f0ad9abb9c925118d69d4852c69f168966
yonasengdu <yonasengdu1994.com>
2022-12-05 16:08:04 +0300

    the logic of generating a password hash, saving a user and hotel datas on a database using a prismaService class, displaying an error when unique values are sent more than once to database and returnig the values of the registerd user execept for the hash value has been handled.

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,79 @@
-import { Injectable } from "@nestjs/common";
+import { ForbiddenException, Injectable } from "@nestjs/common";
 import { PrismaService } from "src/prisma/prisma.service";
+import { hotelAuthDto, userAuthDto } from "./dto";
+import * as argon from "argon2"
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime";
 @Injectable()
-export class AuthService{
+export class AuthService{ 
     constructor(private prisma: PrismaService){}
 
-    signup(){
-        return  {'msg': 'i am signed up'}
+    async userSignup(dto:userAuthDto){
+        //Generate password Hash for the user password
+        const hash = await argon.hash(dto.password);
+        // save the new user in db 
+        try{
+            const user = await this.prisma.user.create({
+                data: {
+                    full_name:dto.full_name,
+                    user_name: dto.user_name,
+                    password_hash:  hash.trim(),
+                    email: dto.email,
+                },
+                select:{
+                    full_name: true,
+                    user_name:true,
+                    password_hash:false,
+                    email:true,
+    
+                }
+            });
+            
+            return user
+        } catch (error){
+            if(error instanceof PrismaClientKnownRequestError){
+                if(error.code === 'P2002'){
+                    throw new ForbiddenException(`the ${error.meta.target} creadential has been taken`)
+                }
+            }
+            throw error
+
+        }
+
+        //return the saved user 
     } 
+
+
+    async hotelSignup(dto:hotelAuthDto){
+         //Generate password Hash for the user password
+         const hash = await argon.hash(dto.password)
+        //  save the user(hotel) in db
+         
+        try {
+                    const hotel = await this.prisma.hotel.create({
+                        data: {
+                            hotel_name: dto.hotel_name,
+                            user_name: dto.user_name,
+                            password_hash:  hash.trim(),
+                            email: dto.email,
+                        },
+                        select:{
+                            hotel_name:true,
+                            user_name:true,
+                            password_hash: false,
+                            email: true,
+                        }
+                    });
+                    return hotel
+        }
+        catch(error){
+            if (error instanceof PrismaClientKnownRequestError){
+                if (error.code === 'P2002'){
+                    throw new ForbiddenException(`the ${error.meta.target} creadential has been taken`)
+                }
+            }
+            throw error
+
+
+        }
+    }
 }

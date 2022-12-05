088efa54f0539bd6468961731df0b600b18eef12
yonasengdu <yonasengdu1994.com>
2022-12-05 16:19:07 +0300

    I removed an optional filed of the prismaClient constructor which is datasources and the url of database file. this filed is removed because the the file location which i wrote in the url filed doen't recognized and was't functioning as it supose to be. removing the optional filed solved the problems i was having.

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,16 +1,17 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
+
 @Injectable()
 export class PrismaService extends PrismaClient {
     constructor(){
-        super({
-          datasources:{
-            db:{
-              url: '../../databases/dev.db'
-    
-            },
-          },
-        }); 
+        super();
+        // super({
+        //   datasources:{
+        //     db:{
+        //       url: 'file:..\\..\\databases\\dev.db'
+        //     },
+        //   },
+        // }); 
       }
 }

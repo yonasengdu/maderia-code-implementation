0236d5c9c0ea4906b8b479de70e75d770ca2a186
yonasengdu <yonasengdu1994.com>
2022-12-05 15:53:22 +0300

    I made the controller module to handle two  separate requests one for 'user' and one for 'hotel'. both requests are post requests in the endpoints 'userSignup' for user and 'hotelSignup' for hotel. for both endpoints the 'AuthController' class in auth.serivce module handles the registration logic through userSignup and hotelSignup functions.

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,17 @@
-import { Controller, Post } from "@nestjs/common";
+import { Body, Controller, Post } from "@nestjs/common";
 import { AuthService } from "./auth.service";
+import { hotelAuthDto, userAuthDto } from "./dto";
 
 @Controller('auth')
 export class AuthController{
     constructor(private authservice: AuthService){}
 
-    @Post('signup')
-    signup(){
-        return this.authservice.signup()
+    @Post('userSignup')
+    userSignup(@Body() dto:userAuthDto){ 
+        return this.authservice.userSignup(dto)
     }
-}    
+   @Post('hotelSignup')
+   hotelSignup(@Body() dto:hotelAuthDto){
+    return this.authservice.hotelSignup(dto)
+   }
+}

2ba90313cf35c37561fbe35caf127b8021b9e9c2
Samuel Birhanu <samuelbirhanu121@gmail.com>
2022-12-15 22:28:51 +0300

    I created templates for the index page, user profile page and hotel profile page. I also configured the back-end code to render these templates instead of returning strings or objects from controllers. The remplates have all the necessary features to make profile update and password reset requests. One thing is left though: currently, the delete-user requests can't be sent from the profile templates because the back-end listens for DELETE requests and there's no convenient way to send DELETE requests from plain HTML templates. The url for the index page is /client/index and the url for the profile pages is /auth/profile

--- src/client/client.controller.ts
+++ src/client/client.controller.ts
@@ -1,22 +1,30 @@
-import { Body, Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Render, Req, UseGuards } from '@nestjs/common';
 import { JwtGuard } from '../auth/jwt.guard';
 import { Request } from 'express';
 import { user } from '@prisma/client';
 
 @Controller('client')
 export class ClientController {
+
   // AuthGuard is a pre-made guard. we tell what type of guard we want by passing 'jwt'
   @UseGuards(JwtGuard)
+  @Render('index')
   @Get('index')
   index(@Req() req: any) {
     // beware! what we get from inside the request can be a user or a hotel. by convention, it's appended as a 'user' in the request.
     const client = req.user;
-    // this is how we know if the client we gor as the 'request.body' is a user or a hotel. we check if
+    // this is how we know if the client we get as the 'request.body' is a user or a hotel. we check if
     // it contains a 'full_name' field only users have that field currently.
     if (client.full_name) {
-      return 'you are ' + req.user.user_name + '. You are logged in as a user.';
+      return {
+        name: client.full_name,
+        client_type: 'user'
+      }
     } else {
-      return 'You are ' + req.user.hotel_name + '. You are logged in as a hotel.';
+      return {
+        name: client.hotel_name,
+        client_type: 'hotel'
+      }
     }
   }
 }

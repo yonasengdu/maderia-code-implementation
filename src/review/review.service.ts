0f47d358b25ec458c6ef3e6c5f53bc299e3c55b5
Biniyamseid <Biniyamseid3@gmail.com>
2023-02-20 10:37:59 +0300

    update review services

--- src/review/review.service.ts
+++ src/review/review.service.ts
@@ -1,4 +1,4 @@
-import { All, Injectable, NotFoundException, UnauthorizedException } from '@nestjs/common';
+import { All, ForbiddenException, Injectable, NotFoundException, UnauthorizedException } from '@nestjs/common';
 import { Prisma } from '@prisma/client';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { reviewDto, updateReviewDto } from './review.dto'
@@ -8,17 +8,34 @@ export class ReviewService {
     constructor(private prisma:PrismaService){}
 
     async createReview(dto: reviewDto, userId: number) {
-        let newReview = await this.prisma.review.create({
-            data: {
-                authorId: userId,
-                hotelId:dto.hotelId,
-                rating:dto.rating,
-                text:dto.text
+        try {
+            let hotel = await this.prisma.hotel.findUnique({
+                where:{
+                    id:dto.hotelId
+                }
+            })
+            if (!hotel){
+                throw  new NotFoundException(`Hotel  with ID ${dto.hotelId} not found.`);  
             }
-        });
-        return newReview
-    }
-
+            let newReview = await this.prisma.review.create({
+                data: {
+                    authorId: userId,
+                    hotelId:dto.hotelId,
+                    rating:dto.rating,
+                    text:dto.text
+                }
+            });
+            return {
+                success:true,
+                data:newReview
+            }
+        } catch (error) {
+            if (error.code === "P2002") {
+                return { success: false, message: "You have already reviewed this hotel." };
+              }
+              return { success: false, message: error.message };
+            }
+}  
     async getAllReviews(){
         let AllReviews = await this.prisma.review.findMany();
         return {
@@ -63,6 +80,62 @@ export class ReviewService {
         
     }
 
-    
+  async deleteReview(id: number, userId: number): Promise<reviewDto> {
+    // Check if review with given id exists
+    const review = await this.prisma.review.findUnique({ where: { id } });
+    if (!review) {
+      throw new NotFoundException(`Review with id ${id} not found`);
+    }
+
+    // Check if the user is the author of the review
+    if (review.authorId !== userId) {
+      throw new ForbiddenException('You are not authorized to delete this review');
+    }
+
+    // Delete the review
+    return this.prisma.review.delete({ where: { id } });
+  }
+
+
+  async getAverageRating(hotelId:number):Promise<number> {
+    const reviews = await this.prisma.review.findMany({
+      where: {
+        hotelId: hotelId,
+      },
+      select: {
+        rating: true,
+      },
+    });
+    if (!reviews){
+        return 0;
+    }
+
+    const numReviews = reviews.length;
+    const sumRatings = reviews.reduce((acc, review) => acc + review.rating, 0);
+
+    if (numReviews === 0) {
+      return 0;
+    } else {
+      const averageRating = sumRatings / numReviews;
+      await this.prisma.hotel.update({
+        where: { id: hotelId },
+        data: { averageRating: averageRating },
+      });
+      return averageRating;
+    }
+  }
+
+  async getAllHotels(){
+    return this.prisma.hotel.findMany()
+  }
 }
+
+
+  
+
+
+
+
+
+
 

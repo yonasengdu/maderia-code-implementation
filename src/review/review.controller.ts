81cc4bb5fd412f18c1907bb6aeebdf1afb01defb
Biniyamseid <Biniyamseid3@gmail.com>
2023-02-20 10:40:46 +0300

    update review controller

--- src/review/review.controller.ts
+++ src/review/review.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Param, ParseIntPipe, Patch, Post, Req, UseGuards } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post, Put, Req, UseGuards } from '@nestjs/common';
 import { JwtGuard } from 'src/auth/jwt.guard';
 import { reviewDto, updateReviewDto } from './review.dto';
 import { ReviewService } from './review.service';
@@ -42,7 +42,21 @@ export class ReviewController {
     }
 
 
-
+    @UseGuards(JwtGuard)
+    @Delete('/:id')
+    async deleteReview(@Param('id', new ParseIntPipe()) reviewId:number, @Req() req: any){
+        return await this.reviewService.deleteReview(reviewId,req.user.id)
+    }
+    @Get('average/:id')
+    async getAverageRating(@Param('id', new ParseIntPipe()) hotelId:number){
+        return await this.reviewService.getAverageRating(hotelId)
+    }
+    //this is controller is added just for testing only. and it is not supposed to be here so it should be in hotels controller.
+    // it's name should be corrected also.
+    @Put('a')
+    async getAllHotels(){
+        return await this.reviewService.getAllHotels()
+    }
 
 
 
